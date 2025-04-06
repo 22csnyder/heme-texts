711533552803140cc408e9efd3393ca28940a400
Christopher Snyder <22csnyder@gmail.com>
2025-04-05 22:51:38 -0500

    Got AI marvin call + named group regex arg passing to work. problem=marvin logger printing to stdout

--- scripts/hw_mrvn.py
+++ scripts/hw_mrvn.py
@@ -1,16 +1,38 @@
 #!../.venv/bin/python
+##Example usage
+# python ./scripts/hw_mrvn.py "Brave, daring, chivalrous, and sometimes a bit reckless."
+
+from dotenv import load_dotenv
+import dotenv
+
+dotenv_file = "/Users/christophersnyder/Projects/heme-texts/.env"
+load_dotenv(dotenv_path=dotenv_file)
+
+example_student = "Brave, daring, chivalrous, and sometimes a bit reckless."
 
 import marvin
+import argparse
 
-student = "Brave, daring, chivalrous, and sometimes a bit reckless."
+parser = argparse.ArgumentParser(description="Classify a student into a Hogwarts house")
+parser.add_argument("student", type=str, help="Description of the student")
 
-def main(): 
-  house = marvin.classify(
-      student,
-      labels=["Gryffindor", "Hufflepuff", "Ravenclaw","Slytherin"]
-  )
-  return house
+
+def ai_hat(student_description):
+    house = marvin.classify(
+        student_description,
+        labels=["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+    )
+    return house
+
+
+ai_hat(example_student)
+
+
+def main(student_description):
+    house = ai_hat(student_description)
+    return house
 
 
 if __name__ == "__main__":
-    print(main())
+    args = parser.parse_args()
+    print(main(args.student))
